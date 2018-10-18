439a39ea33e286bd580750820b0bb598830dfb6e
forblgac <light.island2@gmail.com>
2018-10-19 00:29:59 +0900

    headimg.jpgをcdnからに変更

--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import header from '../img/header.jpg'
 import { Link } from 'gatsby'
 
 const HeaderImage = () => (
 <div>
     <Link to="/">
     <figure className="image">
-        <img src={header} alt="Header" style={{ paddingBottom: -10,paddingTop: 10,paddingRight: 10,paddingLeft: 10}} />
+        <img src='https://ucarecdn.com/c855a677-eb53-4297-8a60-350a65938382/' alt="Header" style={{ paddingBottom: -10,paddingTop: 10,paddingRight: 10,paddingLeft: 10}} />
     </figure>
     </Link>
 </div>
